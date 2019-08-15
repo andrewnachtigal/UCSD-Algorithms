1bab9f2eddc2158e4c00e1b259b3fabc8af0596c
andrewnachtigal <ajnacht@gmail.com>
2019-08-15 10:16:49 -0700

    knapsack

---
+++ 01-algorithmic-design-and-techniques/week-3/fractional-knapsack.py
@@ -0,0 +1,24 @@
+#!/user/bin/python
+
+'''Maximum Value of Loot (Fractional Knapsack)
+Find most valuable combination of items given constraints of capacity,
+weight, and value.
+
+'''
+
+import sys
+
+def get_optimal_value(capacity, weights, values):
+    value = 0.
+    # write your code here
+
+    return value
+
+
+if __name__ == "__main__":
+    data = list(map(int, sys.stdin.read().split()))
+    n, capacity = data[0:2]
+    values = data[2:(2 * n + 2):2]
+    weights = data[3:(2 * n + 2):2]
+    opt_value = get_optimal_value(capacity, weights, values)
+    print("{:.10f}".format(opt_value))
