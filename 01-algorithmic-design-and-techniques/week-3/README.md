d7f2a8fd022dcbc477c4fd2d5b876e1f9abe516b
andrewnachtigal <ajnacht@gmail.com>
2019-08-21 14:31:33 -0700

    dot-product-profit

--- 01-algorithmic-design-and-techniques/week-3/README.md
+++ 01-algorithmic-design-and-techniques/week-3/README.md
@@ -1,4 +1,12 @@
 ### Greedy Algorithms
+
+Greedy algorithm follows the problem solving heuristic of making the locally
+optimal choice at each stage with the intent of finding a global optimum.
+
+Greedy algorithms are similar to dynamic programming algorithms in that the
+solutions are both efficient and optimal if the problem exhibits some particular
+sort of substructure.
+
 * Maximization and Minimization Algorithms
 * Minimum Spanning Trees
 
--- 01-algorithmic-design-and-techniques/week-3/max-dot-product.py
+++ 01-algorithmic-design-and-techniques/week-3/max-dot-product.py
@@ -1,6 +1,6 @@
 #!/user/bin/python
 
-'''Maximizing Advertisement Revenue (Maximize Dot Product)
+'''Maximizing Advertisement Revenue: Dot Product/Sum of Products
 Given two sequences, profit per click of ith ad and average number of clicks
 per day of the i-th slot, partition inot n pairs such that the sum of products
 is maximized.
@@ -11,11 +11,24 @@ is maximized.
 import sys
 
 def max_dot_product(a, b):
-    # code here
-    res = 0
+    result = 0
+
     for i in range(len(a)):
-        res += a[i] * b[i]
-    return res
+        max_a = get_max(a)
+        max_b = get_max(b)
+        result += a[max_a] * b[max_b]
+        a.remove(a[max_a])
+        b.remove(b[max_b])
+    return result
+
+def get_max(arr):
+    max = arr[0]
+    index = 0
+    for i in range(len(arr)):
+        if max < arr[i]:
+            max = arr[i]
+            index = i
+    return index
 
 if __name__ == '__main__':
     input = sys.stdin.read()
