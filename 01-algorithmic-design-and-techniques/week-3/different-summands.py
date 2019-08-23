d84c8652c6006f07f206a51ee1571b56aeec342e
andrewnachtigal <ajnacht@gmail.com>
2019-08-23 13:31:38 -0700

    summands

--- 01-algorithmic-design-and-techniques/week-3/different-summands.py
+++ 01-algorithmic-design-and-techniques/week-3/different-summands.py
@@ -1,7 +1,7 @@
 #!/user/bin/python
 
 '''Maximum Number of Prizes
-represent a given positive integer 𝑛 as a sum of as many pairwise distinct
+Represent a given positive integer 𝑛 as a sum of as many pairwise distinct
 positive integers as possible. That is, to find the maximum 𝑘 such that 𝑛 can
 be written as 𝑎1+𝑎2+···+𝑎𝑘 where 𝑎1,...,𝑎𝑘 are positive integers and 𝑎𝑖 ̸=𝑎𝑗
 for all 1≤𝑖<𝑗≤𝑘.
@@ -13,7 +13,17 @@ import sys
 
 def optimal_summands(n):
     summands = []
-    #write your code here
+    #
+    if n == 1:
+        return [1]
+    for i in range(1, n):
+        if n <= 2 * i:
+            summands.append(n)
+            break
+        else:
+            summands.append(i)
+            n -= i
+
     return summands
 
 if __name__ == '__main__':
