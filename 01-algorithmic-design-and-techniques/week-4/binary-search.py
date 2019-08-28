eb02a8addfdd46bdc93e310485b71248a3906630
andrewnachtigal <ajnacht@gmail.com>
2019-08-27 20:57:08 -0700

    majority-element

--- 01-algorithmic-design-and-techniques/week-4/binary-search.py
+++ 01-algorithmic-design-and-techniques/week-4/binary-search.py
@@ -7,10 +7,24 @@ list is sorted.
 '''
 
 import sys
+import math
 
 def binary_search(a, x):
     left, right = 0, len(a)
     # write your code here
+    if a[0] < x:
+        return -1
+
+    mid = math.floor(a[0] + (a[n] - a[0])/2)
+
+    if x = a[mid]:
+        return mid
+    else:
+
+
+    for i in range(len(a)):
+        if a[i] <= x:
+
 
 def linear_search(a, x):
     for i in range(len(a)):
