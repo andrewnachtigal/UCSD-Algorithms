73f20d65028cd0038f463a6c372e60fa6f59e17b
andrewnachtigal <ajnacht@gmail.com>
2019-11-06 15:38:58 -0800

    string-distance

---
+++ 01-algorithmic-design-and-techniques/week-5/edit-distance.py
@@ -0,0 +1,17 @@
+#!/user/bin/python
+
+'''Compute the Edit Distance Between Two Strings
+
+edit-distance
+
+
+
+'''
+
+# Uses python3
+def edit_distance(s, t):
+    #write your code here
+    return 0
+
+if __name__ == "__main__":
+    print(edit_distance(input(), input()))
