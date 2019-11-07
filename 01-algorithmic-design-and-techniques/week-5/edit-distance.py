5b34a3e513b47689acbb09297c9dc290ab17faec
andrewnachtigal <ajnacht@gmail.com>
2019-11-07 14:59:40 -0800

    edit-dist

--- 01-algorithmic-design-and-techniques/week-5/edit-distance.py
+++ 01-algorithmic-design-and-techniques/week-5/edit-distance.py
@@ -2,16 +2,41 @@
 
 '''Compute the Edit Distance Between Two Strings
 
-edit-distance
-
+The edit-distance between two strings is the minimum number of operations
+(insertions, deletions, and substitutions of symbols) to transform one string
+into another
 
 
 '''
 
 # Uses python3
 def edit_distance(s, t):
-    #write your code here
-    return 0
+    #D[i,0] = i
+    #D[0,j] = j
+    m = len(s)
+    n = len(t)
+    d = []
+
+    for i in range(len(s) + 1):
+        d.append([i])
+    del d[0][0]
+
+    for j in range(len(t) + 1):
+        d[0].append(j)
+
+    for j in range(1, len(t)+1):
+        for i in range(1, len(s)+1):
+            insertion = d[i][j-1] + 1
+            deletion = d[i-1][j] + 1
+            match = d[i-1][j-1]
+            mismatch = d[i-1][j-1] + 1
+            if s[i-1] == t[j-1]:
+                d[i].insert(j, match)
+            else:
+                minimum = min(insertion, deletion, mismatch)
+                d[i].insert(j, minimum)
+    editDist = d[-1][-1]
+    return editDist
 
 if __name__ == "__main__":
     print(edit_distance(input(), input()))
