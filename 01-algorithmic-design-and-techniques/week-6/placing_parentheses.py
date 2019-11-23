088f92bbc5ab49dc567a88d5c2c106def3bb3058
andrewnachtigal <ajnacht@gmail.com>
2019-11-23 13:13:10 -0800

    parentheses

---
+++ 01-algorithmic-design-and-techniques/week-6/placing_parentheses.py
@@ -0,0 +1,26 @@
+#!/user/bin/python
+
+'''Maximizing the Value of an Arithmetic Expression
+
+Placing Parentheses
+
+
+'''
+
+def evalt(a, b, op):
+    if op == '+':
+        return a + b
+    elif op == '-':
+        return a - b
+    elif op == '*':
+        return a * b
+    else:
+        assert False
+
+def get_maximum_value(dataset):
+    #write your code here
+    return 0
+
+
+if __name__ == "__main__":
+    print(get_maximum_value(input()))
