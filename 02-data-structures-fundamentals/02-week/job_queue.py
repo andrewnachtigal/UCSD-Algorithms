def32776fc2a3b45ad478bcad6863686f6165788
andrewnachtigal <ajnacht@gmail.com>
2020-02-22 22:09:17 -0800

    job-queue

--- 02-data-structures-fundamentals/02-week/job_queue.py
+++ 02-data-structures-fundamentals/02-week/job_queue.py
@@ -2,8 +2,17 @@
 
 '''Parallel Processing
 
+Parallelism is the process of processing several set of instructions
+simultaneously. It reduces the total computational time.
 
-'''
+Simulate a program that processes a list of jobs in parallel. Given a list of
+𝑛 independent threads and 𝑚 jobs, threads take jobs in order.
+
+Determine for each job which thread will process it and when will it start
+processing. Apply a priority queue to simulate prcessing of thread events at
+the start and completion of a job.
+
+##################################
 
 from collections import namedtuple
 
@@ -35,3 +44,66 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+'''
+import heapq
+
+class Thread:
+    '''Thread
+
+    Threads sorted by release time.
+    '''
+
+    def __init__(self, thread_id, release_time=0):
+        self.thread_id = thread_id
+        self.release_time = release_time
+
+    def __lt__(self, other):
+        if self.release_time == other.release_time:
+            return self.thread_id < other.thread_id
+        return self.release_time < other.release_time
+
+    def __gt__(self, other):
+        if self.release_time == other.release_time:
+            return self.thread_id > other.thread_id
+        return self.release_time > other.release_time
+
+
+
+class JobQueue:
+
+    def read_data(self):
+        self.num_threads, m = map(int, input().split())
+        self.jobs = list(map(int, input().split()))
+        self.size = len(self.jobs)
+        assert m == len(self.jobs)
+
+    def write_response(self):
+        for thread_id, start_time in self.result:
+            print(thread_id, start_time)
+
+    def assign_jobs(self):
+        '''
+        Assign jobs to threads.
+
+        '''
+        self.result = []
+        self.thread_queue = [Thread(i) for i in range(self.num_threads)]
+
+        for job in self.jobs:
+            thread = heapq.heappop(self.thread_queue)
+
+            self.result.append((thread.thread_id, thread.release_time))
+
+            thread.release_time += job
+            heapq.heappush(self.thread_queue, thread)
+
+
+    def solve(self):
+        self.read_data()
+        self.assign_jobs()
+        self.write_response()
+
+if __name__ == '__main__':
+    job_queue = JobQueue()
+    job_queue.solve()
